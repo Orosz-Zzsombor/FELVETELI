c155d5b6f558a300b73080a5dbb443ebe74ffd4c
Orosz-Zzsombor <101347624+Orosz-Zzsombor@users.noreply.github.com>
2024-01-22 14:04:26 +0100

    Add files via upload

--- Felvétel.xaml.cs
+++ Felvétel.xaml.cs
@@ -23,5 +23,30 @@ namespace FELVETELI
         {
             InitializeComponent();
         }
+
+
+        
+
+        private void btnVissza_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnFelvesz_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sikeres Felvétel");
+        }
+        private void txtRemoveWaterMark(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox box)
+            {
+
+                if (box.Text == "Gipsz Jakab" || box.Text == "example@gmail.com" || box.Text == "Debrecen, Széchenyi u. 58." || box.Text == "62519514862" || box.Text == "2008.10.01" || box.Text == "50")
+                {
+                    box.Text = null;
+                }
+
+            }
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -13,7 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using System.Collections.ObjectModel;
 namespace FELVETELI
 {
     /// <summary>
@@ -27,7 +29,12 @@ namespace FELVETELI
 
 
         }
-        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+
+
+
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); 
+
         private static bool IsTextAllowed(string text)
         {
             return !_regex.IsMatch(text);
@@ -36,6 +43,52 @@ namespace FELVETELI
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void btnFelvetel_Click(object sender, RoutedEventArgs e) 
+        { 
+
+
+            var windowB = new Felvétel();
+            windowB.ShowDialog();
+        }
+
+        private void btnTorol_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = dtgFelveteli.SelectedItem as Diak; if (selectedItem != null) { (dtgFelveteli.ItemsSource as ObservableCollection<Diak>).Remove(selectedItem); }
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            List<Diak> Diak = new List<Diak>();
+            File.ReadAllLines("diakNincs.csv").Skip(1).ToList().ForEach(x => Diak.Add(new Diak(x)));
+            if (dtgFelveteli.Items.Count>0)
+            {
+                if (MessageBox.Show("Diákok felülírása?",
+                "Megerősítés", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+
+                    dtgFelveteli.ItemsSource = Diak;
+                    MessageBox.Show("Sikeres importálás");
+                }
+                else
+                {
+                   
+    
+                 }
+            }
+            else
+            {
+                dtgFelveteli.ItemsSource = Diak;
+            }
+
+
+
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("DiakUj.csv");
         }
     }
 }
